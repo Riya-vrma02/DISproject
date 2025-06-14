f0f4722b4cb081a96d93739217ef40681e751ab4
Riya-vrma02 <riyaverma1stextra@gmail.com>
2025-06-14 23:42:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 5000;
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Ruchika@2327",
+    password: "Login@001",
     database: "food_delivery"
 });
 
